a9cdc1fb2a712d761502b31706f37fb780edd0c6
Ennin Jeter <jeterennin@gmail.com>
2022-05-13 16:49:11 -0500

    Updating App.Js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+import DisplayPlatformStats from '.components/DisplayPlatformStats';
 
 function App() {
+
+  const [videoGames, setVideoGames] = useState([]);
+
+  useEffect(() => {
+    getVideoGames();
+
+  }, []);
+
+  async function getVideoGames(){
+    try{
+      debugger;
+      const response = await axios.get('https://localhost:7260/api/games/');
+      setVideoGames(response.data);
+      } catch(ex){
+        console.log('ERROR in getVideoGames EXCEPTION: ${ex}')
+      }
+  }
   return (
     <div>
-      <h3>Hello World</h3>
+      <DisplayPlatformStats videoGames = {videoGames}/>
     </div>
   );
 }
